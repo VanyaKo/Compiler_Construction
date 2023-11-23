2db9dc1cb7293bf3e0e4a95ea2850bbe6829b035
Nabuki <shichiromail@gmail.com>
2023-11-23 08:58:57 +0300

    add comment

--- compiler/Codegen.cs
+++ compiler/Codegen.cs
@@ -11,6 +11,20 @@ namespace Codegen
 
         // public void Link(List<ClassDeclaration> clss)
         // {
+        // TODO: wrap into custom main class with the default msil entry point, pseudocode:
+        // class TheVeryMainEntryPoint // NOTE: redefining this name in the user code may produce unexpected behaviour
+        // {
+        //     void Main()
+        //     {
+        //         stdin = { create ConsoleStdInAdapter }
+        //         stdout = { create ConsoleStdOutAdapter }
+        //         stderr = { create ConsoleStdErrorAdapter }
+        //         args = { Array([arg.fromStrToIntsArray() for arg in arguments]) }
+
+        //         exec_program = FoundEntryPointClass(stdin, stdout, stderr, args)
+        //         return exec_program.exitCode;
+        //     }
+        // }
         //     // TODO: link stdlib classes
         //     // TODO: create instances of some stdlib classes (stdin/stdout/stderr/program args)
         //     // TODO: find entrypoint
