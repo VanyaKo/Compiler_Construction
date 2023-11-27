2761cee861374945367f1c95f78ca85fbf06a16f
Nabuki <shichiromail@gmail.com>
2023-11-27 19:43:48 +0300

    fix by removing features

--- compiler/stdlib/integer/interface.cs
+++ compiler/stdlib/integer/interface.cs
@@ -32,9 +32,7 @@ namespace OluaStdLibInterfaces
                     
                     // Relations
                     { "less", new MethodInterface { Parameters = new List<TypeName> { OLT.Integer }, ReturnType = OLT.Boolean } },
-                    { "lessEqual", new MethodInterface { Parameters = new List<TypeName> { OLT.Integer }, ReturnType = OLT.Boolean } },
                     { "greater", new MethodInterface { Parameters = new List<TypeName> { OLT.Integer }, ReturnType = OLT.Boolean } },
-                    { "greaterEqual", new MethodInterface { Parameters = new List<TypeName> { OLT.Integer }, ReturnType = OLT.Boolean } },
                     { "equal", new MethodInterface { Parameters = new List<TypeName> { OLT.Integer }, ReturnType = OLT.Boolean } }
                 }
             );
